ebf6d11eee3faab11d2132e6646a08e223553c80
Nicholas Mazzuca <npmazzuca@gmail.com>
2014-11-08 20:32:39 -0800

    Added a full implementation of printf! (only 5 varargs though...)

--- io.s
+++ io.s
@@ -6,17 +6,25 @@ section .text
 
 global _start
 _start:
-    ;read name, 32
+    mov rdi, startup
+    call printf
+
+    read name, 32
 
     mov rdi, hello
     mov rsi, name
     call printf
 
+    mov rdi, equation
+    mov rsi, 42
+    call printf
+
     exit 0
 
 section .bss
-;name resb 32
+name resb 32
 
 section .data
-hello db "Hello, %s. This is a test!", 0xA, 0
-name db "Nicholas", 0
+startup db "What's your name?", 0xA, 0
+hello db "Hello, %s", 0xA, 0
+equation db "The answer to life is %d", 0xA, 0
