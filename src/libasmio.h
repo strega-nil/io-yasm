c0146bcc56c8be9c4a6deb7007a5ab5d8127891c
Nicholas Mazzuca <npmazzuca@gmail.com>
2014-12-29 00:38:11 -0800

    Change from long int to the, correct, uint64_t

--- src/libasmio.h
+++ src/libasmio.h
@@ -1,5 +1,6 @@
 #ifndef LIBASMIO_H
 #define LIBASMIO_H
-void asmio_gets(const char *buf, long int len);
+#include <stdint.h>
+void asmio_gets(const char *buf, uint64_t len);
 void asmio_printf(const char *format, ...);
 #endif
